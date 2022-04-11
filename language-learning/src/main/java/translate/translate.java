acf2ab086f1a7a13becf0b31b7ea6f4a1df29279
aravind <aravind.pamuri@gmail.com>
2022-04-11 09:27:12 -0500

    Translation page

---
+++ language-learning/src/main/java/translate/translate.java
@@ -0,0 +1,93 @@
+package translate;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class translate
+ */
+@WebServlet("/translate")
+public class translate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public translate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		try {
+			String text =  request.getParameter("text");
+			String tgt = request.getParameter("lang");
+			String s = translate_sentence("en", tgt, text);
+			
+			System.out.println("s:::::"+s);
+//			throw new Exception();
+			
+			request.setAttribute("result", s);
+			request.setAttribute("text", text);
+			request.getRequestDispatcher("/translated.jsp").forward(request, response);
+			
+		}
+		catch (Exception e) {
+	         
+	         request.getRequestDispatcher("/translateError.jsp").forward(request, response);
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	
+	private static String translate_sentence(String src, String tgt, String text) throws IOException {
+		
+		System.out.println(src);
+		System.out.println(tgt);
+		System.out.println(text);
+        
+        String urlStr = "https://script.googlre.com/macros/s/AKfycbx0NdKeZknMBbtd1rzoWxEulQOUvfCNeY6GGIXeVHHFBxPv9e6DAWSTH2j_wyFwnszfzw/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + tgt +
+                "&source=" + src;
+        
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        
+        System.out.println(response.toString());
+        return response.toString();
+    }
+
+}
---
+++ language-learning/src/main/webapp/translated.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="te">
+<head>
+<meta charset="UTF-8">
+<title>Translate result</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+
+	<nav class="navbar navbar-inverse navbar-fixed-top ">
+	  <div class="container">
+	    
+      <a class="navbar-brand" href="#">User Home</a>
+      <a class="navbar-brand" href="#">Translate</a>
+      <a class="navbar-brand" href="#">Login</a>
+    </div>
+
+	</nav>
+
+
+	<% String result = request.getAttribute("result").toString(); %>
+	<% String text = request.getAttribute("text").toString(); %>
+	
+		
+	<div class="container">		
+		
+		<div class="jumbotron">
+		
+		
+		  <h1>Original</h1>
+		  <p><%= text %></p>
+		  
+		  <h1>Translated sentence</h1>
+		  <p><%= result %></p>
+		  <p><a class="btn btn-primary btn-lg" href="/translate.html" role="button">Translate Another</a></p>
+		</div>
+		
+	</div>
+</body>
+</html>
